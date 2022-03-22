fee8d5e871983420a83fbb669b35d1f68c2753d2
Robo 🤖 <admin@dunit.co>
2022-03-22 11:42:54 +0100

    Add step component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
 import About from './components/About';
+
+import 'semantic-ui-css/semantic.min.css'
+import Steps from './components/Steps';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Steps />} />
         <Route path="about" element={<About />} />
       </Routes>
     </div>
---
+++ ui/src/components/Steps/index.tsx
@@ -0,0 +1,26 @@
+import { Step } from "semantic-ui-react";
+
+export default function Steps() {
+    return <Step.Group ordered>
+    <Step completed>
+      <Step.Content>
+        <Step.Title>Shipping</Step.Title>
+        <Step.Description>Choose your shipping options</Step.Description>
+      </Step.Content>
+    </Step>
+
+    <Step completed>
+      <Step.Content>
+        <Step.Title>Billing</Step.Title>
+        <Step.Description>Enter billing information</Step.Description>
+      </Step.Content>
+    </Step>
+
+    <Step active>
+      <Step.Content>
+        <Step.Title>Confirm Order</Step.Title>
+      </Step.Content>
+    </Step>
+  </Step.Group>
+}
+
