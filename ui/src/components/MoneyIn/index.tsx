11dabd950ad7ff3640ee0ea42ce91dbc0bca9735
Robo 🤖 <admin@dunit.co>
2022-03-25 12:38:51 +0100

    add charts

--- ui/src/components/MoneyIn/index.tsx
+++ ui/src/components/MoneyIn/index.tsx
@@ -1,8 +1,57 @@
 import _ from "lodash";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Container, Table } from "semantic-ui-react";
 import { LedgerRow } from "../../App"
 import AddMoneyInModal from "../AddMoneyInModal";
+import styled from "styled-components";
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ChartData,
+  } from 'chart.js';
+  import { Bar } from 'react-chartjs-2';
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  export const options = {
+    plugins: {
+      title: {
+        display: false,
+        text: 'Chart.js Bar Chart - Stacked',
+      },
+    },
+    // responsive: true,
+    maintainAspectRatio : false,
+    scales: {
+      x: {
+        stacked: true,
+        gridLines: {
+            display:false
+        }
+      },
+      y: {
+        stacked: true,
+        gridLines: {
+            display:false
+        }
+      },
+    },
+  };
+
+  const labels = _.range(28).map((n) => { return (n+1)})
 
 interface MoneyInProps {
     ledger: Array<LedgerRow>
@@ -10,17 +59,56 @@ interface MoneyInProps {
     removeLedgerRow: (id: string) => void 
 }
 
+const graphDataInitialState = {
+    labels,
+    datasets: [],
+  };
+
 export default function MoneyIn(props: MoneyInProps) {
     const [addMoneyInModalOpen, setAddMoneyInModalOpen] = useState<boolean>(false);
+    const [sortedLedger, setSortedLedger] = useState<LedgerRow[]>([]);
+    const [graphData, setGraphData] = useState<ChartData<"bar", number[], unknown>>(graphDataInitialState);
     const { ledger, addLedgerRow } = props;
     
-    const sortedLedger = _.orderBy(ledger, ["dayOfMonth"], ["asc"])
+    useEffect(() => {
+        const data = {
+            labels,
+            datasets: [
+              {
+                label: 'Penger Inn',
+                data: labels.map((l) => sortedLedger.find((i) => i.dayOfMonth === l && i.accountTo === 'user')?.amount || 0),
+                backgroundColor: 'rgb(255, 99, 132)',
+              },
+            //   {
+            //     label: 'Dataset 2',
+            //     data: labels.map((l) => sortedLedger.find((i) => i.dayOfMonth === l)?.dayOfMonth || 0),
+            //     backgroundColor: 'rgb(75, 192, 192)',
+            //   },
+            //   {
+            //     label: 'Dataset 3',
+            //     data: labels.map((l) => sortedLedger.find((i) => i.dayOfMonth === l)?.dayOfMonth || 0),
+            //     backgroundColor: 'rgb(53, 162, 235)',
+            //   },
+            ],
+          };
+
+          setGraphData(data);
+      }, [sortedLedger]);
+
+    useEffect(() => {
+        setSortedLedger(_.orderBy(ledger, ["dayOfMonth"], ["asc"]));
+      }, [ledger]);
+
 
     return <Container>
 
         { addMoneyInModalOpen && <AddMoneyInModal open={addMoneyInModalOpen} setOpen={setAddMoneyInModalOpen} addLedgerRow={addLedgerRow}   /> }
 
         <h1>Penger Inn</h1>
+
+        <StyledGraphContainer>
+            <Bar options={options} data={graphData} />
+        </StyledGraphContainer>
 
         <Button onClick={() => {
             setAddMoneyInModalOpen(true);
@@ -52,3 +140,7 @@ export default function MoneyIn(props: MoneyInProps) {
         </Table>
     </Container>
 }
+
+const StyledGraphContainer = styled.div`
+    height: 100px;
+`
