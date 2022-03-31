38a541fccd0f2b8b729bd177b3699983f9584dd5
Pierre Larsson <942448+pjelar@user.noreply.github.com>
2022-03-31 17:37:31 +0100

    Adding test and pr pipeline

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Router, Navigator } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Router location={''} navigator={undefined}>
+      <App />
+    </Router>
+  
+  );
+  const linkElement = screen.getByText(/Familieoversikt/i);
   expect(linkElement).toBeInTheDocument();
 });
