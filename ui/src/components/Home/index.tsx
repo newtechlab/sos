cc34643d3869357143e5ed3105d7c1ea2b495561
Anne Karen Aanonli <anne.karen.aanonli@dnb.no>
2022-05-01 10:15:41 +0200

    landing page start

--- ui/src/components/Home/index.tsx
+++ ui/src/components/Home/index.tsx
@@ -1,73 +1,131 @@
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import { useNavigate } from "react-router-dom";
-import { Button, Container } from "semantic-ui-react";
+import { Button, Container, Modal, Image, Icon } from "semantic-ui-react";
 import styled from "styled-components";
 import { FamilyMember, LedgerRow } from "../../App";
 import PdfHandler from "../../services/PdfService/PdfService";
 import { StyledBoxSection } from "../StyledBoxSection";
+import frontpage_family from "./frontpage_family.png";
 
 export interface HomProps {
-    setFamilyMembers: (_: Array<FamilyMember>) => void
-    setLedger: (_: Array<LedgerRow>) => void
+  setFamilyMembers: (_: Array<FamilyMember>) => void;
+  setLedger: (_: Array<LedgerRow>) => void;
 }
 
 export const firstStep = "/family";
 
 export default function Home(props: HomProps) {
-    const navigate = useNavigate();
-    const { setFamilyMembers, setLedger } = props; 
-    const onDrop = useCallback(acceptedFiles => {
-        const fileReader = new FileReader();  
-        fileReader.onload =  async (event) => {
+  const navigate = useNavigate();
+  const { setFamilyMembers, setLedger } = props;
+  const onDrop = useCallback((acceptedFiles) => {
+    const fileReader = new FileReader();
+    fileReader.onload = async (event) => {
+      if (event?.target?.readyState === FileReader.DONE) {
+        const typedarray = new Uint8Array(event.target.result as ArrayBuffer);
+        const pdfHandler = new PdfHandler(typedarray);
+        const attachments = await pdfHandler.getAttachments();
+        const attachmentsAsObject = attachments.map((a) => {
+          const decoded = new TextDecoder().decode(a.data);
+          return JSON.parse(decoded);
+        });
 
-            if (event?.target?.readyState === FileReader.DONE) {
-                const typedarray = new Uint8Array(event.target.result as ArrayBuffer);
-                const pdfHandler = new PdfHandler(typedarray);
-                const attachments = await pdfHandler.getAttachments();
-                const attachmentsAsObject = attachments.map((a) => {
-                    const decoded = new TextDecoder().decode(a.data);
-                    return JSON.parse(decoded)
-                })
-                
-                attachmentsAsObject.map((attachment) => {
-                    if (attachment.familyMembers) {
-                        const fm = attachment.familyMembers as Array<FamilyMember>
-                        setFamilyMembers(fm);
-                    }
+        attachmentsAsObject.map((attachment) => {
+          if (attachment.familyMembers) {
+            const fm = attachment.familyMembers as Array<FamilyMember>;
+            setFamilyMembers(fm);
+          }
 
-                    if (attachment.ledger) {
-                        const ledger = attachment.ledger as Array<LedgerRow>
-                        setLedger(ledger)
-                    }
-                })
-            }   
-            navigate(firstStep);                    
-        };
-        fileReader.readAsArrayBuffer(acceptedFiles[0]);
+          if (attachment.ledger) {
+            const ledger = attachment.ledger as Array<LedgerRow>;
+            setLedger(ledger);
+          }
+        });
+      }
+      navigate(firstStep);
+    };
+    fileReader.readAsArrayBuffer(acceptedFiles[0]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-      }, [])
-      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  return (
+    <Container>
+      <StyledBoxSection>
+        <Image size="large" src={frontpage_family} wrapped />
+        <Styledtitle>Økonomiveilederen</Styledtitle>
 
-    return <Container>
-        <StyledBoxSection>
-            
-            <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-                isDragActive ?
-                <StyledDragParagraph>Drag a previous session here ...</StyledDragParagraph> :
-                <StyledDragParagraph>Drag 'n' drop some files here, or click to select files</StyledDragParagraph>
-            }
-            </div>
-            
-            <Button onClick={() => {navigate(firstStep)}}>Start new</Button>
-            
-        </StyledBoxSection>
+        <p>
+          Velkommen til økonomiveilederen! Målet med dette verktøyet er å gi deg
+          bedre oversikt over din pengebruk. Ved å få bedre oversikt, tror vi at
+          det blir enklere å ta nødvendige grep for å få en trygg økonomi og en
+          enklere hverdag.{" "}
+        </p>
+
+        <StyledNBParagraph>
+          <StyledIcon>
+            <Icon name="info circle" />
+          </StyledIcon>
+          NB! Før vi begynner ber vi deg gjøre klar Bank-ID da vi vil be deg om
+          å logge på forskjellige nettsteder for å få full oversikt over din
+          økonomisk situasjon
+        </StyledNBParagraph>
+
+        <StyledSpace {...getRootProps()}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <StyledDragParagraph>
+              Drag a previous session here ...
+            </StyledDragParagraph>
+          ) : (
+            <StyledDragParagraph>
+              <Icon name="cloud upload" color="blue" /> Dra og slipp PDF eller
+              klikk i feltet for å åpne fra maskin.
+            </StyledDragParagraph>
+          )}
+        </StyledSpace>
+        <StyledSpace>eller</StyledSpace>
+        <StyledSpace>
+          <Button
+            color={"blue"}
+            onClick={() => {
+              navigate(firstStep);
+            }}
+          >
+            Begynn ny kartlegging
+          </Button>
+        </StyledSpace>
+      </StyledBoxSection>
     </Container>
+  );
 }
 
+const StyledSpace = styled.div`
+  padding: 1em !important;
+`;
+
 const StyledDragParagraph = styled.p`
-    background-color: #F1F8F8;
-    height: 40px;
-`
+  background-color: #f1f8f8 !important;
+  border: 1px dashed #3d8eb1;
+  padding: 3em;
+  border-radius: 3px;
+`;
+
+const Styledtitle = styled.h1`
+  font-weight: bold !important;
+  padding: 1em;
+`;
+const StyledNBParagraph = styled.p`
+  background-color: #f1f8f8 !important;
+  padding: 0.5em;
+  text-align: left;
+  border-radius: 3px;
+`;
+const StyledIcon = styled.p`
+  background-color: #f1f8f8 !important;
+  color: #3d8eb1;
+  padding: 0.5em;
+  text-align: left;
+  font-size: 17px;
+  border-radius: 2px;
+  float: left;
+`;
