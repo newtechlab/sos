454ceb1a7be998e0713f80ad18ee764d44394f88
Robo 🤖 <admin@dunit.co>
2022-05-03 13:26:52 +0100

    Add goals and improve sliders

--- ui/src/components/Slider/index.tsx
+++ ui/src/components/Slider/index.tsx
@@ -1,29 +1,52 @@
 import { useState } from "react";
+import { Grid, Icon } from "semantic-ui-react";
 import styled from "styled-components";
 
 interface SliderProps {
   id: string;
+  maxPercent: string;
   onUpdateValue: (id: string, value: string) => void
 }
 
 export const Slider = (props: SliderProps) => {
-  const { id, onUpdateValue } = props
+  const { id, onUpdateValue, maxPercent } = props
   const [slider, setSlider] = useState<string>("100");
 
   return (
     <div className="slidecontainer">
-      <StyledSlider
-        type="range"
-        min="1"
-        max="100"
-        value={slider}
-        className="slider"
-        id="myRange"
-        onChange={(e) => {
-          setSlider(e.target.value);
-          onUpdateValue(id, e.target.value)
-        }}
-      />
+      <Grid>
+        <Grid.Column width={1}>
+          <Icon name="minus circle" onClick={() => {
+            const newValue = (parseInt(slider) - 1).toString()
+            onUpdateValue(id, newValue);
+            setSlider(newValue);
+          }}/>  
+        </Grid.Column>
+        <Grid.Column width={14}>
+          <StyledSlider
+            type="range"
+            min="1"
+            max={maxPercent}
+            value={slider}
+            className="slider"
+            id="myRange"
+            onChange={(e) => {
+              setSlider(e.target.value);
+              onUpdateValue(id, e.target.value)
+            }}
+          />
+        </Grid.Column>
+        <Grid.Column width={1}>
+          <Icon name="plus circle" onClick={() => {
+            const newValue = (parseInt(slider) + 1).toString()
+            onUpdateValue(id, newValue);
+            setSlider(newValue);
+          }}/>
+        </Grid.Column>
+      
+      </Grid>
+
+      
     </div>
   );
 };
