d20685fdfc7cdf230cf91117b247b5fbc33c1e0c
Pierre Larsson <942448+pjelar@user.noreply.github.com>
2022-05-19 11:42:56 +0100

    Using transition slider

--- ui/src/components/HelpTextModalGoal/index.tsx
+++ ui/src/components/HelpTextModalGoal/index.tsx
@@ -1,35 +1,55 @@
 import styled from "styled-components";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Modal, TransitionablePortal } from "semantic-ui-react";
+import { Fragment } from "react";
 
 interface AddHelpTextModalProps {
   open: boolean;
   setOpen: (_: boolean) => void;
 }
 
+// export const DEFAULT_PROPS = {
+//   transition: {
+//     animation: "scale",
+//     duration: 300
+//   }
+// };
+
 export default function OpenHelpTextModal(props: AddHelpTextModalProps) {
   const { open, setOpen } = props;
+  const transition = {
+    transition: {
+      animation: "slide left",
+      duration: 1500
+    }
+  };  
 
   return (
-    <Modal
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      open={open}
-      trigger={<LabelButton>Hvorfor spør vi om dette?</LabelButton>}
-    >
-      <Modal.Header>Hva mener vi med sparemål?</Modal.Header>
+    <Fragment>
 
-      <Modal.Actions>
-        <p>
-          Med sparemål så mener vi om du har noe du sparer til? Har du en drøm
-          om å kjøpe en bil, ta med familien på ferie eller noe annet som krever
-          at du må spare penger? Ønsker du ganske enkelt bare å spare litt til
-          framtiden? Legg inn et beløp som vi kan hjelpe deg å nå.
-        </p>
-        <Button circular basic color="blue" onClick={() => setOpen(false)}>
-          Lukk
-        </Button>
-      </Modal.Actions>
-    </Modal>
+    <LabelButton onClick={() => setOpen(true)}>Open Modal</LabelButton>
+    <TransitionablePortal {...{ open }} {...{ transition }}>
+      <Modal
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        trigger={<LabelButton>Hvorfor spør vi om dette?</LabelButton>}
+      >
+        <Modal.Header>Hva mener vi med sparemål?</Modal.Header>
+
+        <Modal.Actions>
+          <p>
+            Med sparemål så mener vi om du har noe du sparer til? Har du en drøm
+            om å kjøpe en bil, ta med familien på ferie eller noe annet som krever
+            at du må spare penger? Ønsker du ganske enkelt bare å spare litt til
+            framtiden? Legg inn et beløp som vi kan hjelpe deg å nå.
+          </p>
+          <Button circular basic color="blue" onClick={() => setOpen(false)}>
+            Lukk
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </TransitionablePortal>
+    </Fragment>
   );
 }
 const LabelButton = styled.button`
@@ -45,3 +65,5 @@ const LabelButton = styled.button`
     text-decoration: underline;
   }
 `;
+
+// Modal.defaultProps = DEFAULT_PROPS;
