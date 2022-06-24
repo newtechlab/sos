a878ef1b38cdea575a6634195a03a75e6668618d
Pierre Larsson <942448+pjelar@user.noreply.github.com>
2022-06-24 09:05:07 +0100

    Test hash routes https://www.freecodecamp.org/news/deploy-a-react-app-to-github-pages/

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,14 +2,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import "typeface-montserrat";
 import "typeface-roboto";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
